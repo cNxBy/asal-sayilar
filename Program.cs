f72604acfee874725c31ff9e584b9400e5dba61e
erdur <erdur@CNOBY>
2024-11-20 23:57:45 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿namespace asal_sayılar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Asal sayıları belirle!");
+            int a, b;
+            int kontrol;
+            Console.WriteLine("*** Bir sayı giriniz ***");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("*** Bir sayı giriniz ***");
+            b=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            if (a == 1) 
+            {
+                a++;
+            }
+            if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    kontrol = 0;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            kontrol++;
+                            break;
+                        }
+
+                    }
+                    if (kontrol == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            
+            
+               
+            
+            else if (b < a) 
+            {
+                if (b == 1) 
+                {
+                    b++;
+                }
+                for (int i = b; i <= a; i++)
+                { 
+                    kontrol = 0;
+                    for(int j = 2;j < i; j++)
+                    {
+                        if(i % j == 0) 
+                        {
+                            kontrol++;
+                            break;
+                        }
+                    }
+if(kontrol == 0) {
+                        Console.WriteLine(i); 
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
